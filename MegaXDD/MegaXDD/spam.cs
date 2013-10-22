3ec445e28d9a2df17435a17a2de889834388390a
KingSam <mrdickhead@live.com.au>
2013-10-22 20:44:12 +1100

    start
    the begining

---
+++ MegaXDD/MegaXDD/spam.cs
@@ -0,0 +1,14 @@
+﻿/* types of houses
+1 Shed (0 fame) (start with it) 
+2 Cottage (10000)
+3 home (15000)
+4 big home (25000)
+5 mansion (50000)
+6 villa (75000)
+7 Manor (100000)
+*/
+/* decorations
+with decorations i was thinking there would be shit like fountains and statues/graves
+house customization colouring shit like that
+in some of the higher level houses you could have shit like portals straing to the realm
+*/
